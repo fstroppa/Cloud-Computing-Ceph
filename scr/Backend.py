f50c6f6c3f21182c035be1fd9f468dbad9662f28
fstroppa <73309095+fstroppa@users.noreply.github.com>
2021-06-25 17:03:24 +0200

    Added add object, delete object, get object and get cluster state.

--- scr/Backend.py
+++ scr/Backend.py
@@ -19,10 +19,50 @@ class HandleServer:
         except Exception as e:
             print("Unable to get the object list from: " + self.pool)
 
+    def add_object(self, file):
+        ioctx = self.cluster.open_ioctx(self.pool)
+        with open(file) as f:
+            file_content_in_binary = str.encode(f.read())
+        try:
+            ioctx.write_full(file, file_content_in_binary)
+            print("{} was successfully added.".format(file))
+        except Exception as e:
+            print(e)
+            print("Unable to add the object to: " + self.pool)
+        ioctx.close()
+
+    def delete_object(self, file):
+        ioctx = self.cluster.open_ioctx(self.pool)
+        ioctx.remove_object(file)
+        print("{} was successfully deleted.".format(file))
+        ioctx.close()
+
+    def get_object(self, file):
+        ioctx = self.cluster.open_ioctx(self.pool)
+        # TODO Correct the length
+        file_content = ioctx.read(file, length=int(10e12)).decode("utf-8")
+        print(file_content)
+        ioctx.close()
+
     def create_pool_if_non_existent(self, pool="exam_data"):
         if self.cluster.pool_exists(pool) is False:
             self.cluster.create_pool(pool)
         self.pool = pool
 
+    def get_cluster_state(self):
+        io_context = self.cluster.open_ioctx(self.pool)
+        status = io_context.get_stats()
+        print("Clusters Status: \n")
+        for key, value in status.items():
+            print("{}: {} \n".format(key, value))
+
 server = HandleServer()
 server.handle_request(server.get_object_list)
+# server.handle_request(server.delete_object, 'la_divina_comedia.txt')
+# server.handle_request(server.get_object_list)
+server.handle_request(server.add_object, 'CentoMB.txt')
+server.handle_request(server.get_object_list)
+# server.handle_request(server.get_object, 'CentoMB.txt')
+# server.handle_request(server.get_cluster_state)
+
+
